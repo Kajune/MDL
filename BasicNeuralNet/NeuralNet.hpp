62c81d4ff18d262fa2f12fe4f913cc868402cf20
Ichimaru <Ichimaru@DESKTOP-CIMP5DP>
2017-04-21 16:51:09 +0900

    Back Propagation

--- BasicNeuralNet/NeuralNet.hpp
+++ BasicNeuralNet/NeuralNet.hpp
@@ -2,6 +2,7 @@
 
 #include "Vector.hpp"
 #include <cmath>
+#include <random>
 
 template <typename T>
 struct StepFunction {
@@ -17,10 +18,25 @@ struct SigmoidFunction {
 	}
 };
 
-template <size_t Input, size_t Output, template <typename DataType> class OutputFunction, typename DataType = float>
+template <typename T>
+struct DerivedSigmoidFunction {
+	T operator()(const T& rhs) {
+		return static_cast<T>(rhs * (1.0 - rhs));
+	}
+};
+
+template <size_t Input, size_t Output, 
+	template <typename DataType> class OutputFunction,
+	template <typename DataType> class DerivedOutputFunction,
+	typename DataType = float>
 class Perceptron {
 	std::array<Vector<Input, DataType>, Output> m_weight;
 	Vector<Output, DataType> m_threshold;
+	DataType m_learningRate = 1.0;
+
+	Vector<Input, DataType> m_input;
+	mutable bool m_isChanged = true;
+
 public:
 	Perceptron() = default;
 	Perceptron(const Perceptron&) = default;
@@ -28,15 +44,58 @@ public:
 	Perceptron& operator=(const Perceptron&) = default;
 	Perceptron& operator=(Perceptron&&) = default;
 
-	Perceptron(const std::array<Vector<Input, DataType>, Output>& weight, const Vector<Output, DataType>& threshould)
-		: m_weight(weight), m_threshold(threshould){
+	Perceptron(DataType learningRate)
+		: m_learningRate(learningRate) {
 	}
 
-	Vector<Output, DataType> Forward(const Vector<Input, DataType>& input) const {
-		Vector<Output, DataType> ret;
-		OutputFunction<DataType> func;
+	Perceptron(DataType learningRate,
+		const std::array<Vector<Input, DataType>, Output>& weight, 
+		const Vector<Output, DataType>& threshould)
+		: m_learningRate(learningRate), m_weight(weight), m_threshold(threshould){
+	}
+
+	void SetRandom(DataType min, DataType max) {
+		std::random_device rd;
+		std::mt19937 mt(rd());
+		std::uniform_real_distribution<DataType> dist(min, max);
 		for (size_t i = 0; i < Output; i++) {
-			ret.at(i) = func(DotProduct(input, m_weight.at(i)) - m_threshold.at(i));
+			for (size_t j = 0; j < Input; j++) {
+				m_weight.at(i).at(j) = dist(mt);
+			}
+			m_threshold.at(i) = dist(mt);
+		}
+	}
+
+	void SetInput(const Vector<Input, DataType>& input) {
+		m_input = input;
+		m_isChanged = true;
+	}
+
+	Vector<Output, DataType> Forward(const Vector<Input, DataType>& input) {
+		SetInput(input);
+		return Forward();
+	}
+
+	Vector<Output, DataType> Forward() const {
+		static Vector<Output, DataType> ret;
+		if (m_isChanged) {
+			OutputFunction<DataType> func;
+			for (size_t i = 0; i < Output; i++) {
+				ret.at(i) = func(DotProduct(m_input, m_weight.at(i)) - m_threshold.at(i));
+			}
+			m_isChanged = false;
+		}
+		return ret;
+	}
+
+	Vector<Input, DataType> Backward(const Vector<Output, DataType>& error) {
+		Vector<Input, DataType> ret;
+		DerivedOutputFunction<DataType> func;
+		for (size_t i = 0; i < Output; i++) {
+			auto d = error.at(i) * func(Forward().at(i));
+			m_weight.at(i) += m_input * m_learningRate * d;
+			m_threshold.at(i) -= m_learningRate * d;
+			ret += m_weight.at(i) * d;
 		}
 		return ret;
 	}
--- BasicNeuralNet/main.cpp
+++ BasicNeuralNet/main.cpp
@@ -1,11 +1,39 @@
 #include "NeuralNet.hpp"
 #include <iostream>
 
-int main() {
-	Perceptron<2, 2, StepFunction> p1({ Vector<2>{-2, 3}, Vector<2>{-2, 1} }, { -1.0, 0.5 });
-	Perceptron<2, 1, StepFunction> p2({ Vector<2>{-60, 94} }, { -1.0 });
-	Vector<2> vec{ 0, 0 };
-	auto ret = p2.Forward(p1.Forward(vec));
+std::pair<Vector<2>, float> TrainingData[] = {
+	std::make_pair(Vector<2>{1, 1}, 0.0f),
+	std::make_pair(Vector<2>{1, 0}, 1.0f),
+	std::make_pair(Vector<2>{0, 1}, 1.0f),
+	std::make_pair(Vector<2>{0, 0}, 0.0f),
+};
 
-	std::cout << ret.at(0) << std::endl;
+int main() {
+	Perceptron<2, 3, SigmoidFunction, DerivedSigmoidFunction> p1(10);
+	Perceptron<3, 1, SigmoidFunction, DerivedSigmoidFunction> p2(10);
+
+	p1.SetRandom(-5, 5);
+	p2.SetRandom(-5, 5);
+	
+	float errorcount;
+	int count = 0;
+	do {
+		errorcount = 0.0f;
+		for (const auto& it : TrainingData) {
+			p1.Backward(p2.Backward(Vector<1>{ it.second } -p2.Forward(p1.Forward(it.first))));
+			errorcount += powf(it.second - p2.Forward().at(0), 2.0f);
+		}
+		std::cout << "Error: " << errorcount << std::endl;
+		count++;
+	} while (errorcount > 0.0001f);
+
+	std::cout << "Learn Complete" << std::endl;
+
+	while (true) {
+		float a, b, c;
+		std::cin >> a >> b;
+		p1.SetInput({ a, b });
+		p2.SetInput(p1.Forward());
+		std::cout << p2.Forward().at(0) << std::endl;
+	}
 }
